deb8f13229dad8c3c19e8cb4d7c8a845bbf45857
kri <krivchenya1998@mail.ru>
2018-07-14 14:45:30 +0300

    login page

--- pages/Register.js
+++ pages/Register.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+
+//const {width, height} = Dimension.get('window');
 
 const Register = () => {
+    const login = () => {
+        alert('idi nahoooooooooi' + this.text)
+    }
     return (
-        <View><Text>Register</Text></View>
+        <View>
+            <Text style={styles.text}>Register</Text>
+            <TextInput placeholder='name' onChangeText={(e)=>{this.text = e}} style={styles.textInput}/> 
+            <TextInput placeholder='age' style={styles.textInput}/>
+            <Button onPress={login} color='#24ad2e' style={styles.button} title='Log In' />    
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    text: {
+        textAlign: 'center',
+        fontSize: 30, 
+        marginBottom: 20,
+    },
+    textInput: {
+        width: 240,
+        height: 40,
+        marginBottom: 30,
+    },
+    button: {
+        backgroundColor: 'green'
+    }
+  });
 
 export default Register;
