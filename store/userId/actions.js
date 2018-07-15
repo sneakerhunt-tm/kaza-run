738bb92763141e9a964d843fb00623fe9f86652a
unknown <k.smolniy@gmail.com>
2018-07-15 12:26:21 +0300

    finish registration actions

--- store/userId/actions.js
+++ store/userId/actions.js
@@ -17,9 +17,37 @@ export function createUser(dispacth, model) {
         const userId = {id: res.name, role: model.role};
         return AsyncStorage.setItem('accountId',JSON.stringify(userId)).then(() => {
             return saveImageInBase64(model.role === 0 ? 'runners' : 'jduns', userId.id, model.img).then((imgAns) => {
-                dispacth({ type: CREATE_USER, data: userId });
-                return userId;
+                return importAccountToMatchBase(model.role, userId.id).then(() => {
+                    dispacth({ type: CREATE_USER, data: userId });
+                    return userId;
+                })
             });
         })
+    })
+}
+
+export function importAccountToMatchBase(roleId, id) {
+    const antiRole = roleId !==  0 ? 'runners' : 'jduns';
+    const role = roleId ==  0 ? 'runners' : 'jduns';
+
+    return request(antiRole).then(res => {
+        const antiRoleIds = Object.keys(res);
+        const patchObject = antiRoleIds.reduce((acc, curr) => {
+            acc[antiRole][curr] = {
+                [id]: {
+                    checked: false,
+                    match: false,
+                }
+            };
+            acc[role][id] = {
+                [curr] : {
+                    checked: false,
+                    match: false,
+                }
+            }
+            return acc;
+        }, { [antiRole]: {}, [role]: {}});
+
+        return request('matches','PATCH', patchObject);
     })
 }
